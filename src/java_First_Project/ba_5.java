d538a8158581216908beb9943928ec230b27bb73
echong22 <tdc8165@naver.com>
2024-12-27 23:55:06 +0900

    fds

---
+++ src/java_First_Project/ba_5.java
@@ -0,0 +1,24 @@
+package java_First_Project;
+
+import java.util.Scanner;
+
+public class ba_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		waior w1 = new waior();
+	
+		w1.power = 20;
+		w1.name = "전사1";
+		w1.Color = "초록색";
+		
+		System.out.println("-- 전사 정보 --");
+		System.out.println("키 : " +  w1.power);
+		System.out.println("키 : " +  w1.name);
+		System.out.println("키 : " +  w1.Color);
+
+	
+	}
+
+}
---
+++ src/java_First_Project/waior.java
@@ -0,0 +1,11 @@
+package java_First_Project;
+
+import java.util.Scanner;
+
+public class waior {
+	int heigt; // 키
+	int power; // 공격력
+	
+	String Color; // 참조 타입 "안녕하세요" [문자열]
+	String name;
+}
